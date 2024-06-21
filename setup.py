7cc4d141a011fd2eb352b4a9337e44cec88b3fbe
mikkel <mikkel@255bits.com>
2024-06-21 16:30:32 -0600

    update attribution

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup, find_packages
 setup(
     name="comfyui_api",
     version="0.1.0",
-    author="NTC Slider Factory",
-    author_email="aiden.summers@ntcsliderfactory.com",
+    author="255Labs.xyz",
+    author_email="",
     description="A Python API for interacting with ComfyUI",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/ntcsliderfactory/comfyui_api",
+    url="",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
